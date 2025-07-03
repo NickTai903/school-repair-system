07250a25498de9dd9c00fbeb0ffcf4a8e5533f68
NickTai903 <u970130189@gmail.com>
2025-07-03 22:57:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -59,6 +59,8 @@ app.get('/api/repairs/:id', (req, res) => {
   });
 });
 
-app.listen(3001, () => {
-  console.log('Server running on http://localhost:3001');
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
+
